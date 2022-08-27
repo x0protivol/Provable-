96435371a614078a8f2e2fae8b3bc6fcd003284a
x0protivol <94541118+x0protivol@users.noreply.github.com>
2022-08-27 18:36:20 +0530

    Add files via upload

---
+++ packages/react-app/src/components/LiteralChip/CategoryIcon.js
@@ -0,0 +1,29 @@
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import YardIcon from '@mui/icons-material/Yard';
+import TableChartIcon from '@mui/icons-material/TableChart';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import { categories } from './../../tags';
+
+export function CategoryIcon({ category }) {
+  switch (category) {
+    case categories.finance:
+      return <PriceCheckIcon />;
+    case categories.gaming:
+      return <VideogameAssetIcon />;
+    case categories.poap:
+      return <WorkspacePremiumIcon />;
+    case categories.human:
+      return <VerifiedIcon />;
+    case categories.lens:
+      return <YardIcon />;
+    case categories.tableland:
+      return <TableChartIcon />;
+    case categories.medicine:
+      return <MedicalServicesIcon />;
+    default:
+      return null;
+  }
+}
